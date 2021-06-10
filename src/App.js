b79436e2b51f8cffbff3105668bed76c947c8328
raphaelnicolini <raphaelnicolini1991@gmail.com>
2021-06-10 10:12:39 -0300

    filtro2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import Filtros from './components/Filtros';
 
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div>
-      <Filtros />
-    </div>
-  );
+import Carrinho from './components/Carrinho';
+import Produtos from './components/Produtos';
+
+// import CarrinhoLogic from '.  /logic/carrinho'
+
+
+const Container = styled.div`
+  display: flex;
+`;
+
+// Lembrar de passar carrinhoLogic com props
+// let carrinhoLogic = new CarrinhoLogic();
+
+function Inicio () {
+  
+    return (
+      <Container>
+        <Filtros />
+        <Produtos/>
+        <Carrinho/>
+      </Container>
+    )
+  
 }
 
-export default App;
+export default Inicio()
