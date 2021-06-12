07daf468a565d1c1e04fbd73495acf79d1785af8
elainicristina <elaini@tuta.io>
2021-06-12 13:07:18 -0300

    imagens dos planetas

--- src/App.js
+++ src/App.js
@@ -57,43 +57,41 @@ const NomeSite = styled.h1`
 
 const listaProdutos = [
   {
-    imagemProduto: "https://blog.enem.com.br/wp-content/uploads/2020/04/img-materia-r7-inter.jpg",
+    imagemProduto: Miller,
     nomeProduto: "Planeta Miller",
     valor: 85,
     id: 1
   },
   {
-    imagemProduto: "https://gizmodo.uol.com.br/wp-content/blogs.dir/8/files/2019/04/buraco-negro-gargantua-970x444.jpg",
+    imagemProduto: Gargantua,
     nomeProduto: "Buraco Negro Gargantua",
     valor: 90,
     id: 2
   },
   {
-    imagemProduto: "https://blogdoishii.files.wordpress.com/2018/07/k01vra.jpg",
+    imagemProduto: Ranger,
     nomeProduto: "Planeta Ranger",
     valor: 55,
     id: 3
   },
   {
-    imagemProduto: "https://imagens.publico.pt/imagens.aspx/885789?tp=UH&db=IMAGENS&type=JPG",
+    imagemProduto: Pandora,
     nomeProduto: "Planeta Pandora",
     valor: 75,
     id: 4
   },
   {
-    imagemProduto: "https://super.abril.com.br/wp-content/uploads/2020/01/agua-marte-curiosity.jpg",
+    imagemProduto: Marte,
     nomeProduto: "Planeta Marte",
     valor: 20,
     id: 5
   },
   {
-    imagemProduto: "https://cdn.universoracionalista.org/wp-content/uploads/2020/06/ceres-dawn.jpg",
+    imagemProduto: Asteroid,
     nomeProduto: "Asteróide B-612",
     valor: 32,
     id: 6
   }
-
-
 ];
 
 export default class App extends React.Component {
